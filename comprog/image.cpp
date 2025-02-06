16aaa37425aca4a08c71903387ce92c7d60bccc3
OninDynamics <onindynamics@pm.me>
2025-02-06 23:34:46 +0800

    finish whatever that lasang is

--- comprog/image.cpp
+++ comprog/image.cpp
@@ -2,21 +2,21 @@
 
 int main() {
 
-    // Declare a completely white 3x3 image.
+    // Declare a 3x3 image.
     uint8_t img[3][3][3] = 
     {
         {
-            { 0xff, 0xff, 0xff }, { 0xff, 0xff, 0xff }, { 0xff, 0xff, 0xff }
+            { 0xff, 0xff, 0xff }, { 0xb6, 0xb6, 0xb6}, { 0x46, 0x46, 0x46 }
         },
         {
-            { 0xff, 0xff, 0xff }, { 0xff, 0xff, 0xff }, { 0xff, 0xff, 0xff }
+            { 0xf8, 0x5a, 0x5a }, { 0xc5, 0x08, 0x08 }, { 0x45, 0x03, 0x03 }
         },
         {
-            { 0xff, 0xff, 0xff }, { 0xff, 0xff, 0xff }, { 0xff, 0xff, 0xff }
+            { 0x5a, 0xee, 0xf8 }, { 0x08, 0xb6, 0xc5 }, { 0x03, 0x41, 0x45 }
         },
     };
 
-    // Example: Lower the brightness by 50%
+    // Example 1: Lower the brightness by 50%
     for (char row=0; row < 3; row++) {                          // For each row of pixels...
         for (char pixel=0; pixel < 3; pixel++) {                // For each pixel...
             for (char channel = 0; channel < 3; channel++) {    // Every color channel...
@@ -25,13 +25,13 @@ int main() {
         }
     }
 
-    // Example: Add a green filter
-    for (char row=0; row < 3; row++) {                          // For each row of pixels...
-        for (char pixel=0; pixel < 3; pixel++) {                // For each pixel...
-            img[row][pixel][0] /= 2;                            // Halve the Red channel!
-            img[row][pixel][1] /= 2;                            // Halve the Green channel!
-        }                                                       // Because the blue channel was untouched...
-    }                                                           // The image becomes blue!
+    // Example 2: Add a blue filter
+    for (char row=0; row < 3; row++) {              // For each row of pixels...
+        for (char pixel=0; pixel < 3; pixel++) {    // For each pixel...
+            img[row][pixel][0] /= 2;                // Halve the Red channel!
+            img[row][pixel][1] /= 2;                // Halve the Green channel!
+        }                                           // Because the blue channel was untouched...
+    }                                               // The image becomes blue!
 
     // Example: Lower the brightness by 50%
     for (char row=0; row < 3; row++) {                          // For each row of pixels...
