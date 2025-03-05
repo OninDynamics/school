c158704a8d5f85b4d254710e92052ee3ff8d5650
OninDynamics <onindynamics@pm.me>
2025-03-05 22:43:52 +0800

    finish recursion examples

--- comprog/recursion examples.cpp
+++ comprog/recursion examples.cpp
@@ -30,8 +30,42 @@ ll_node<T>* ll_end(ll_node<T>* node) {
 
 
 
+#define MAX_ITERATIONS 100	// Limit to amount of iterations needed
+#define LIMIT 5				// If Z exceeds a distance than LIMIT to the
+							// origin (0,0), C is not part of the set.
 
-#DEFINE MAX_ITERATIONS 1000
+#include <complex>			// Use the <complex> library for complex numbers
 
-// If the point is part of the mandelbro
-bool 
+using namespace std;
+
+// If the point C exceeds value, consider it not part of the mandelbrot set, so
+// return false. If max iterations are reached without exceeding LIMIT,
+// consider point as part of set, so return true.
+bool mandel(complex<double> z, complex<double> c, unsigned int iters) {
+
+	// Check if max iterations achieved, and z is still within limit
+	if (iters >= MAX_ITERATIONS && abs(z) < LIMIT ) {
+		return true;
+	
+	// If z ever exceeds LIMIT, immediately return false
+	} else if ( abs(z) > LIMIT ) {
+		return false;
+	}
+
+	// If iteration limit still not achieved, continue iteration
+
+	z = (z*z) + c;	// Equation of the Mandelbrot Set
+	return mandel(z, c, iters++);
+}
+
+// The mandel() function should be called as follows:
+complex<double> point;
+bool is_part_of_set = mandel( complex<double>(0,0), point, 0 );
+// ...where "point" contains the complex point being tested.
+
+
+
+
+
+
+
